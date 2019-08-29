c9e93d8b7e006aaba34ceff0c4889132cbc27cac
marinus <marinus@laurabo.dk>
2019-08-29 18:13:41 +0200

    lol k

--- kurv.js
+++ kurv.js
@@ -1,44 +1,40 @@
 /*
  * Dette script definerer klassen Kurv
 */
+class Kurv {
+    constructor(x, y, bredde, dybde, speed) {
+        /* Den første del af funktionen er en "konstruktør".
+         * Den tager parametrene og konstruerer et nyt objekt
+         * ud fra dem. Værdierne huskes som hørende til netop
+         * dette objekt ved hjælp af nøgleordet this
+         */
+        this.x = x;
+        this.y = y;
+        this.bred = bredde;
+        this.dyb = dybde;
+        this.speed = speed;
+        this.col = [250, 230, 150];
+    }
 
-function Kurv(x, y, bredde, dybde, speed) {
-    /* Den første del af funktionen er en "konstruktør".
-     * Den tager parametrene og konstruerer et nyt objekt 
-     * ud fra dem. Værdierne huskes som hørende til netop 
-     * dette objekt ved hjælp af nøgleordet this
-     */
-    
-     //Skaber variablerne
-    this.x = x;
-    this.y = y;
-    this.bred = bredde;
-    this.dyb = dybde;
-    this.speed = speed;
-    this.col = [250,230,150];
+    tegn() {
 
-    //tegner kurven
-    img = loadImage('turban.png');
-    this.tegn = function() {
+        if (keyIsDown(65)) { // A
+            this.x = Math.max(this.x - this.speed, 0);
+        }
+        if (keyIsDown(68)) { // D
+            this.x = Math.min(this.x + this.speed, width - this.bred);
+        }
         fill(this.col);
-      //rect(this.x, this.y, this.bred, this.dyb);
-        image(img, this.x, this.y, this.bred, this.dyb, 90);
+        rect(this.x, this.y, this.bred, this.dyb);
     }
 
-    //kurven bevæger sig med tasterne
-    this.move = function(tast) {
-        this.x = mouseX;
-        this.y = mouseY;
-    }
-
-    //hvis den er grebet starter den forfra
-    this.grebet = function(xa, ya, ra) {
-        if ((ya < this.y+80 && ya > this.y-30) && xa > this.x+ra && xa < this.x+this.bred-ra) {
+    grebet(x, y) {
+        if ((y < this.y + this.dyb + rad && y > this.y + rad) && x > this.x + rad && x < this.x + this.bred - rad) {
             return true;
         }
         else {
             return false;
         }
     }
+}
 
-} 
--- sketch.js
+++ sketch.js
@@ -3,167 +3,189 @@ Først laver vi et nogle variable til at lave en appelsin
  - en kugle som vi vil skyde afsted og fange i en turban
 */
 
-// Appelsinen
-var x = 0;
-var y = 550;
-var rad = 20;
-var xspeed = 4;
-var yspeed = -10;
-var newspeed = yspeed;
-var grav = 0.1;
-var col = [200, 100, 0];
-var missed = 0
-var header;
-var spill = true;
-var buttion;
-
-
-// Turbanen
+// Turban variable
 var turban;
+// Header/Title variable
+var header;
 
 // Øvrige
+var tid = 10;
 var score = 0;
 var missed = 0;
+var spil = true;
+
+//laver Appelsin arrayed
 var appelsiner = [];
 
+//laver variablen til socket
+var socket;
+
 function setup() {
-    header = createElement('h1', 'Appelsiner i turban')
-    var status = document.getElementById("status");
-    status.innerHTML = 'Lorte spillet er igang';
-    createCanvas(750, 600);
-    newspeed = yspeed;
-    x = rad;
-    turban = new Kurv(670, 100, 70, 50, 10);
-    appelsiner.push(new appelsin(670, 100, 70, 50, 10));
-    //
-    setInterval(function () {
-        appelsiner.push(new appelsin(670, 100, 70, 50, 10))
-    }, 3000);
+
+
+    if (missed < 3) {
+        var status = document.getElementById('status');
+        status.innerHTML = 'Lorte spillet er i gang';
+        createCanvas(windowWidth, windowHeight);
+        header = createElement('h1', 'Appelsiner i turban')
+        x = rad;
+        turban = new Kurv(windowWidth / 2, windowHeight - 100, 150, 50, 10);
+
+        if (confirm('Vil du joine et igangværende spil?')) {
+            var pin = prompt('Pin:');
+            socket = ElineSocket.connect(pin);
+        } else {
+            socket = ElineSocket.create();
+        }
+        socket.onMessage(handleMessage);
+    } else {
+
+        status.createButton('Restart?')
+        
+
+    }
+}
+
+function handleMessage(msg) {
+    switch (msg.type) {
+        case 'shootNew':
+            shootNew(msg.x);
+            break;
+        default:
+            console.log('Unknown message', msg);
+    }
+}
+
+function shootNew(x) {
+    //Her skal vi sørge for at en ny appelsin skydes afsted
+    const dx = 6 * Math.random();
+    const dy = -15 * Math.random();
+    appelsiner.push(new Appelsin(x, dx, dy));
+}
+
+function move() {
+    for (var i = 0; i < appelsiner.length; i++) {
+        const appelsin = appelsiner[i];
+
+        if (appelsin.isOutOfBounds()) {
+            appelsiner.splice(i, 1);
+            i--;
+        } else {
+            appelsin.move();
+        }
+        if (appelsin.y > windowHeight+10){
+            missed += 1;
+        }
+    }
+}
+
+function checkScore() {
+    for (var i = 0; i < appelsiner.length; i++) {
+        const appelsin = appelsiner[i];
+        if (appelsin.dy > 0) {
+            if (turban.grebet(appelsin.x, appelsin.y)) {
+                score += 1;
+                appelsiner.splice(i, 1);
+                i--;
+            }
+        }
+    }
+}
+
+function display() {
+    background(0);
+
+    for (var i = 0; i < appelsiner.length; i++) {
+        const appelsin = appelsiner[i];
+        appelsin.display();
+    }
+
+    // Her vises turbanen - foreløbig blot en firkant
+    turban.tegn();
+
+    fill(255)
+    text("Score: " + score, width - 80, 30);
+    text("Pin: " + socket.id, width - 80, 60);
+    text("Missed: " + missed, width - 80, 90);
 }
 
 function draw() {
 
-  
+    if (spil) {
+ 
+    background(0);
+    move();
+    checkScore();
+    display();
 
-    if (spill) {
-        background(0);
-        for (var i = 0; i < appelsiner.length; i++) {
-
-            appelsiner[i].move();
-            appelsiner[i].appelsin();
-
-
-        }
-        checkScore();
-        display();
-        turban.move();
-    } else {
-
+    }else{
+     
         document.getElementById('status').innerHTML = 'Game Over';
-        button = createButton('Genstart');
-        button.position(350, 375);
+        button = createButton('Restart Game');
+        button.position(300, 325);
         button.mousePressed(restartGame);
     }
-
-    if (missed > 1) {
-        spill = false;
+    if (missed > 1){
+        spil = false;
     }
 
-}
-
-function restartGame() {
-    location.reload();
-}
-
-function display() {
-    fill(255);
-    text("Score: " + score, width - 80, 30);
-    fill(255)
-    text("Missed: " + missed, width - 80, 50);
-
-    //Her skal vi sørge for at appelsinen bliver vist, hvis den skal vises
-
-
-    // Her vises turbanen - foreløbig blot en firkant
-    turban.tegn();
-}
-
-function checkScore() {
-    // Her checkes om turbanen har fanget appelsinen. Hvis ja, skydes en ny appelsin afsted
-    if (yspeed > 10) {
-        if (turban.grebet(x, y, rad)) {
-            score += 1;
-            appelsiner.shift()
-        }
-    }
-    for (var i = 0; i < appelsiner.length; i++) {
-
-        if (turban.grebet(appelsiner[i].x, appelsiner[i].y, appelsiner[i].rad)) {
-            score += 1;
-            appelsiner.shift()
-        }
-
-    }
 
 }
 
-
-
-function keyPressed() {
-
-}
-
-function mousePressed() {
-
+function mouseClicked() {
+    socket.sendMessage({
+        type: 'shootNew',
+        x: mouseX,
+    });
 }
 
 /*
 OPGAVER
  Opgave 1 - undersøg hvad variablerne  grav  og  tid  bruges til.
             Skriv det i kommentarer, prøv at se hvad der sker, når
-            I laver dem om. 
+            I laver dem om.
 
- Opgave 2 - lav programmet om så det er tilfældigt hvor højt oppe 
-            på venstre kan appelsinerne starter. Overvej om man kan 
-            sikre, at appelsinen ikke ryger ud af skærmens top men 
+ Opgave 2 - lav programmet om så det er tilfældigt hvor højt oppe
+            på venstre kan appelsinerne starter. Overvej om man kan
+            sikre, at appelsinen ikke ryger ud af skærmens top men
             stadig har en "pæn" bane
 
  Opgave 3 - lav programmet om så man også kan bevæge turbanen mod
             højre og venstre med A- og D-tasterne. Prøv jer frem med
             forskellige løsninger for hvor hurtigt turbanen skal rykke
 
- Opgave 4 - ret programmet til, så det også angives hvor mange 
+ Opgave 4 - ret programmet til, så det også angives hvor mange
             appelsiner man IKKE greb med turbanen
 
- Opgave 5 - Undersøg hvad scriptet  kurv.js  er og gør, samt hvad de 
-            funktioner, scriptet indeholder, skal bruges til. Skriv 
+ Opgave 5 - Undersøg hvad scriptet  kurv.js  er og gør, samt hvad de
+            funktioner, scriptet indeholder, skal bruges til. Skriv
             det som kommentarer oven over hver funktion. Forklar tillige,
             hvad sammenhængen mellem dette script og turbanen i hoved-
-            programmet er, og forklar det med kommentarer i toppen af 
+            programmet er, og forklar det med kommentarer i toppen af
             kurv.js
 
- Opgave 6 - find et billede af en turban og sæt det ind i stedet 
-            for firkanten. Find eventuelt også en lyd, der kan afspilles, 
-            når appelsinen gribes. Se gerne i "p5 Reference" hvordan, 
+ Opgave 6 - find et billede af en turban og sæt det ind i stedet
+            for firkanten. Find eventuelt også en lyd, der kan afspilles,
+            når appelsinen gribes. Se gerne i "p5 Reference" hvordan,
             hvis ikke I kan huske det:   https://p5js.org/reference/
             Lav programmet om, så man kan flytte turbanen med musen
 
- Opgave 7 - lav en Appelsin-klasse, lige som der er en Kurv-klasse. 
+ Opgave 7 - lav en Appelsin-klasse, lige som der er en Kurv-klasse.
             Flyt koden til appelsinen ud i et selvstændigt script.
-            Overvej hvad det skal hedde, oghvilke variabler og funktioner, 
-            der skal lægges over i det nye script, herunder hvordan det 
-            kommer til at berøre turbanen. Skriv jeres overvejelser i 
+            Overvej hvad det skal hedde, oghvilke variabler og funktioner,
+            der skal lægges over i det nye script, herunder hvordan det
+            kommer til at berøre turbanen. Skriv jeres overvejelser i
             kommentarerne
 
- Opgave 8 - ret programmet til, så der kan være flere appelsiner i 
+ Opgave 8 - ret programmet til, så der kan være flere appelsiner i
             luften på en gang, dvs. at der kan skydes en ny appelsin
-            afsted før den foregående er forsvundet. Overvej hvordan 
+            afsted før den foregående er forsvundet. Overvej hvordan
             og hvor hurtigt de skal skydes af, og forklar jeres tanker
             i kommentarerne
 
  Opgave 9 - ret programmet til, så det kan vindes og/eller tabes ved
-            at man griber eller misser et antal appelsiner. Sørg for 
-            at der vises en "Game Over"-skærm, som fortæller om man 
+            at man griber eller misser et antal appelsiner. Sørg for
+            at der vises en "Game Over"-skærm, som fortæller om man
             vandt eller tabte, og som giver mulighed for at starte et
             nyt spil.
 
